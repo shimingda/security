ca437906e16e93ccc3735676b89af52bf6e7929d
shimingda <2575624555@qq.com>
2019-03-28 15:53:48 +0800

    mod:修改过滤文件

--- src/main/java/com/dome/filter/CorsFilter.java
+++ src/main/java/com/dome/filter/CorsFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 跨域
+ * 处理跨域问题
  */
 @Component
 @Slf4j
@@ -17,10 +17,9 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT,DELETE,HEAD");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        System.out.println("*********************************执行过滤器**************************");
         chain.doFilter(req, res);
     }
     public void init(FilterConfig filterConfig) {}
